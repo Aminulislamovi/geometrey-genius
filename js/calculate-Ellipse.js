7906a32ccb1335663e034eaa40524d5dfcbe8ef8
Aminulislamovi <md.aminul4480@gmail.com>
2024-04-02 00:31:07 +0600

    added calculate Ellipse are

---
+++ js/calculate-Ellipse.js
@@ -0,0 +1,7 @@
+function calculateEllipseArea (){
+ const majorRadius = getInputValueById ('major-radius')
+   const minorRadius = getInputValueById ('minor-radius')
+   const area = 3.14 * majorRadius * minorRadius ;
+    setInnerTextById ('ellipse-area',area)
+}
+
